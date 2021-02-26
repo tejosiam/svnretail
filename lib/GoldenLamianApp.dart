9da0eb94075126ba69c5a5520f3516e109344680
tejosiam <fdtejo@gmail.com>
2021-02-26 14:59:51 +0700

    MVC Clean

---
+++ lib/GoldenLamianApp.dart
@@ -0,0 +1,16 @@
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello Ali'),
+        ),
+      ),
+    );
+  }
+}
