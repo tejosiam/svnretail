7411032427b6dba4e984a70ac0d4063dc17716a7
Narancia <alhabsyir@gmail.com>
2021-04-21 14:28:52 +0700

    update from beta to stable branch ali;

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\PCHO21T005\dashboard\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\FlutterDev\Golden-Lamian-Store-Tab\svnretail"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Aplikasi\Golden-Lamian-Store-Tab\svnretail"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
